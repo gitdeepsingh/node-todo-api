d4eb8bdb0b3898e0323a3b197306c33c97f9d638
Deep Singh <deepsingh.cts@gmail.com>
2018-06-03 12:54:17 +0530

    added  get todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todo) => {
+    res.send({ todo });
+  }, (err) => {
+    res.status(400).send(err)
+  })
+})
+
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
